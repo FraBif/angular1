b1f02b3eecfec71b587e313440fa5249a83ae770
FraBif <francescobif@hotmail.it>
2023-11-10 21:23:37 +0100

    created components and installed materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProvaComponent } from './prova/prova.component';
+import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvaComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/prova/prova.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  templateUrl: './prova.component.html',
+  styleUrl: './prova.component.css',
+})
+export class ProvaComponent implements OnInit{
+  constructor(){}
+   ngOnInit(): void {
+       
+   }
+}
